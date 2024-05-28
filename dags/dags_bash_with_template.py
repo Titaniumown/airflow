e22730ff52ba255061e9705e78c408001ea23df6
Titaniumown <fhwlxpr33@gmail.com>
2024-05-28 13:51:21 +0900

    오타

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -19,7 +19,7 @@ with DAG(
         task_id = 'bash_t2',
         env={
             'START_DATE':'{{data_interval_start | ds}}',
-            'END_DATe':'{{data_interval_end}}'
+            'END_DATE':'{{data_interval_end}}'
         },
         bash_command='echo $START_DATE && echo $END_DATE'
     )
