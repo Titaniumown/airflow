4a55fcbcbf9bea098cc8016040b45447769af6f2
Titaniumown <fhwlxpr33@gmail.com>
2024-06-20 16:00:26 +0900

    task.branch 데코레이터 DAG 작성2

--- dags/dags_python_with_branch_decorator.py
+++ dags/dags_python_with_branch_decorator.py
@@ -20,23 +20,24 @@ with DAG(
         elif selected_item in ['B', 'C']:
             return ['task_b', 'task_c']
         
-def common_func(**kwargs):
-        print(kwargs['selected'])
+    def common_func(**kwargs):
+            print(kwargs['selected'])
+        
+    task_a=PythonOperator(
+        task_id='task_a',
+        python_callable=common_func,
+        op_kwargs={'selected':'A'}
+    )
     
-task_a=PythonOperator(
-    task_id='task_a',
-    python_callable=common_func,
-    op_kwargs={'selected':'A'}
-)
-
-task_b=PythonOperator(
-    task_id='task_b',
-    python_callable=common_func,
-    op_kwargs={'selected':'B'}
-)    
-task_c=PythonOperator(
-    task_id='task_c',
-    python_callable=common_func,
-    op_kwargs={'selected':'C'}
-)
-select_random() >> [task_a, task_b, task_c]
+    task_b=PythonOperator(
+        task_id='task_b',
+        python_callable=common_func,
+        op_kwargs={'selected':'B'}
+    )    
+    task_c=PythonOperator(
+        task_id='task_c',
+        python_callable=common_func,
+        op_kwargs={'selected':'C'}
+    )
+    
+    select_random() >> [task_a, task_b, task_c]
