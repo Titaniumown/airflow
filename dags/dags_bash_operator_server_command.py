7ffe56da5e99719d96043540fdef8975708003fe
Titaniumown <fhwlxpr33@gmail.com>
2025-02-13 10:45:22 +0900

    d

--- dags/dags_bash_operator_server_command.py
+++ dags/dags_bash_operator_server_command.py
@@ -13,7 +13,10 @@ with DAG(
     # [START howto_operator_bash]
     bash_t1 = BashOperator(
         task_id="bash_t1",
-        bash_command="df -h ",
+        bash_command="""
+        whoami
+        pwd
+        """,
     )
 
     # bash_t2 = BashOperator(
